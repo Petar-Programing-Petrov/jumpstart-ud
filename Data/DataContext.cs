0fad32c53c85018f08cb04ba278d69bce7f107bf
Petar <petar.dragov.petrov@gmail.com>
2022-10-14 10:45:59 +0200

    FEAT: Added EF and created first migration to the SQL Database

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using jumpstart_ud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace jumpstart_ud.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Character> Characters { get; set; }
+    }
+}
+ 
