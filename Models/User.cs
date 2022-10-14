02ec0d9c1ace05a6f5d4681fdebe3086473ccc0f
Petar <petar.dragov.petrov@gmail.com>
2022-10-14 14:14:43 +0200

    FEAT: Added a user Model

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace jumpstart_ud.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = String.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}
