0a680b114c55f4e64b6c20a454e272205569685e
Petar <petar.dragov.petrov@gmail.com>
2022-10-17 11:39:57 +0200

    UPDATE: Creating a Token and using it for register and Login

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,44 +1,39 @@
 ﻿
 using jumpstart_ud.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace jumpstart_ud.Data
 {
+
+    //Dependency Injection 
     public class AuthRepository : IAuthRepository
     {
         private readonly DataContext _context;
+        private readonly IConfiguration _config;
 
-        public AuthRepository(DataContext context)
+        public object SymetricSecutiryKey { get; private set; }
+
+        public AuthRepository(DataContext context , IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
-        public async Task<ServiceResponse<string>> Login(string username, string password)
-        {
-            var response = new ServiceResponse<string>();
 
-            var user = await _context.Users
-                .FirstOrDefaultAsync(u => u.Username.ToLower().Equals(username.ToLower()));
-
-            if (user == null)
-            {
-                response.Success = false;
-                response.Message = "User not found";
-            }
-            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
-            {
-                response.Success = false;
-                response.Message = "Wrong password!";
-            }
-            else
-            {
-                response.Data = user.Id.ToString();
-            }
+       
 
 
-            return response;
-        }
 
+        /// <summary>
+        /// Registration method
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        /// <returns>ServiceResponse response.Success and response.Message if an error ocures</returns>
         public async Task<ServiceResponse<int>> Register(User user, string password)
         {
 
@@ -62,14 +57,44 @@ namespace jumpstart_ud.Data
             return response;
         }
 
-        public async Task<bool> UserExists(string username)
+
+
+        /// <summary>
+        /// Login method
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public async Task<ServiceResponse<string>> Login(string username, string password)
         {
-            if (await _context.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower()))
+            var response = new ServiceResponse<string>();
+
+            //Find the user in Db
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Username.ToLower().Equals(username.ToLower()));
+
+            if (user == null)
             {
-                return true;
+                //If the user is not the same as in Database
+                response.Success = false;
+                response.Message = "User not found";
             }
-            return false;
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                //If the pasword is wrong
+                response.Success = false;
+                response.Message = "Wrong password!";
+            }
+            else
+            {
+                //Create a Token for the user
+                response.Data = CreateToken(user);
+            }
+
+
+            return response;
         }
+
 
 
         /// <summary>
@@ -88,6 +113,41 @@ namespace jumpstart_ud.Data
         }
 
 
+
+        /// <summary>
+        /// Create a token
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+
+            return tokenHandler.WriteToken(token);
+        }
+
+
+
+        //Password verification
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
             using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
@@ -95,6 +155,16 @@ namespace jumpstart_ud.Data
                 var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                 return computeHash.SequenceEqual(passwordHash);
             }
+        }
+
+        //Check if User exsits
+        public async Task<bool> UserExists(string username)
+        {
+            if (await _context.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower()))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
