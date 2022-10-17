18f43bb5dac9250d65f4d181fb0884bd74aa5878
Petar <petar.dragov.petrov@gmail.com>
2022-10-17 10:48:49 +0200

    UPDATE: Authentication endpoint added and working

--- Data/IAuthRepository.cs
+++ Data/IAuthRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace jumpstart_ud.Data
+﻿using jumpstart_ud.Models;
+
+namespace jumpstart_ud.Data
 {
     public interface IAuthRepository
     {
+        Task<ServiceResponse<int>> Register(User user, string password);
+
+        Task<ServiceResponse<string>> Login(string username, string password);
+
+        Task<bool> UserExists(string username);
     }
 }
