12a9a6c110cbc841bb06c27f1563be70a8d1a7b6
Petar <petar.dragov.petrov@gmail.com>
2022-10-18 11:00:42 +0200

    FEAT: Weapon class WIP

--- Models/Character.cs
+++ Models/Character.cs
@@ -12,7 +12,10 @@
         public int Defense { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
 
+        public User? User { get; set; }
         public RpgClass Class { get; set; } = RpgClass.Knight;
+
+        public Weapon Weapon { get; set; }
 
     }
 }
---
+++ Models/Weapon.cs
@@ -0,0 +1,10 @@
+﻿namespace jumpstart_ud.Models
+{
+    public class Weapon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public int Damage { get; set; }
+        public Character Character { get; set; }
+    }
+}
