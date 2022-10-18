cac9b10e10a29d72e534553f6f6fa02c64220472
Petar <petar.dragov.petrov@gmail.com>
2022-10-18 11:12:02 +0200

    FEAT: Added Weapons in DBset to create a new table with relation to character and weapon WIP

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -5,13 +5,14 @@ namespace jumpstart_ud.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) 
+        public DataContext(DbContextOptions<DataContext> options)
             : base(options)
         {
 
         }
         public DbSet<Character> Characters { get; set; }
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Weapon> Weapons { get; set; }
     }
 }
- 
